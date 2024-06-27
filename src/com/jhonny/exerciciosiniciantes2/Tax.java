1d856dbb5b8012a53caccd30a2c712f2cc0d01c7
Jhonny <jhonnyms1012@gmail.com>
2024-06-27 09:11:46 -0300

    feat: Concluído exercício 5

--- src/com/jhonny/exerciciosiniciantes2/Tax.java
+++ src/com/jhonny/exerciciosiniciantes2/Tax.java
@@ -9,18 +9,22 @@ public class Tax {
 		Scanner scanner = new Scanner(System.in);
 		
 		double salary = scanner.nextDouble();
+		double taxAmount = 0;
 		
-		
-		
-		if(salary <= 2000) {
-			System.out.println("Insento");
-		}else if(salary > 2000 && salary < 3000) {
-			salary = (salary - 2000) * 0.8;
-			System.out.println(salary);
-		}else if (salary > 3000 && salary < 4500) {
-			
+		if(salary > 4500) {
+			taxAmount =+ (salary - 4500) * 0.28;
+			salary = 4500;
 		}
-		
+		if(salary > 3000) {
+			taxAmount =+ (salary - 3000) * 0.18;
+			salary = 3000;
+		}
+		if(salary > 2000) {
+			taxAmount += (salary - 2000) * 0.08;
+		}else {
+			System.out.println("Insento");
+		}
+		System.out.printf("R$ %.2f%n", taxAmount);
 		
 		
 		scanner.close();
